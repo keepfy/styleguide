05d61fb144818981c955418ce30880c0a6b7261a
JeovaZERO <jeovapereira2000@live.com>
2019-12-18 23:19:27 +0000

    chore: add preset types

---
+++ src/presets.ts
@@ -0,0 +1,20 @@
+export enum Feedback {
+    danger = 'danger',
+    warning = 'warning',
+    success = 'success',
+    info = 'info'
+}
+
+export enum Priority {
+    critical = 'critical',
+    high = 'high',
+    medium = 'medium',
+    low = 'low',
+    minimal = 'minimal'
+}
+
+export enum Criticality {
+    high = 'high',
+    medium = 'medium',
+    low = 'low'
+}
